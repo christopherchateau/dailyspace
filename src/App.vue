f2b31eca2ad4680f443e68ee5d8d4cef553cf7bb
chris <christopherchateau@gmail.com>
2019-01-08 15:55:26 -0700

    Add stand alone button to App.vue instead of footer element

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div class="app">
     <Header/>
     <Main/>
-    <Footer/>
+    <button v-on:click="handleClick">{{ buttonDisplay }}</button>
   </div>
 </template>
 
@@ -13,6 +13,18 @@ import Footer from "./components/Footer.vue";
 
 export default {
   name: "app",
+  data() {
+    return {
+      buttonDisplay: "today"
+    };
+  },
+  methods: {
+    handleClick: function() {
+      this.buttonDisplay === "today"
+        ? (this.buttonDisplay = "this month")
+        : (this.buttonDisplay = "today");
+    }
+  },
   components: {
     Header,
     Main,
@@ -26,7 +38,162 @@ export default {
 
 .app {
   display: grid;
+  font-family: "Rubik", sans-serif;
   grid-auto-rows: 150px 1fr 150px;
   height: 100vh;
 }
 </style>
+
+
+// export const mockDailes = [
+//   {
+//     title: "Pillars of Creation",
+//     url:
+//       "https://cdn.spacetelescope.org/archives/images/wallpaper2/heic1501a.jpg"
+//   },
+//   {
+//     title: "A Rose Made of Galaxies",
+//     url:
+//       "https://cdn.spacetelescope.org/archives/images/wallpaper2/heic1107a.jpg"
+//   },
+//   {
+//     title: "Extreme Star Cluster Burst",
+//     url:
+//       "https://cdn.spacetelescope.org/archives/images/wallpaper2/heic0715a.jpg"
+//   },
+//   {
+//     title: "The Bubble Nebula",
+//     url:
+//       "https://cdn.spacetelescope.org/archives/images/wallpaper2/heic1608a.jpg"
+//   },
+//   {
+//     title: "Antennae Galaxies",
+//     url:
+//       "https://cdn.spacetelescope.org/archives/images/wallpaper2/potw1345a.jpg"
+//   },
+//   {
+//     title: "Horsehead Nebula",
+//     url:
+//       "https://cdn.spacetelescope.org/archives/images/wallpaper2/heic1307a.jpg"
+//   },
+//   {
+//     title: "NGC 1275",
+//     url:
+//       "https://cdn.spacetelescope.org/archives/images/wallpaper2/heic0817a.jpg"
+//   },
+//   {
+//     title: "Galaxies Galore",
+//     url:
+//       "https://cdn.spacetelescope.org/archives/images/wallpaper2/heic0406a.jpg"
+//   },
+//   {
+//     title: "Sombrero Galaxy",
+//     url:
+//       "https://cdn.spacetelescope.org/archives/images/wallpaper2/opo0328a.jpg"
+//   },
+//   {
+//     title: "Shedding Light on the Past",
+//     url:
+//       "https://cdn.spacetelescope.org/archives/images/wallpaper2/heic0702a.jpg"
+//   },
+//   {
+//     title: "Crab Nebula",
+//     url:
+//       "https://cdn.spacetelescope.org/archives/images/wallpaper2/heic0515a.jpg"
+//   },
+//   {
+//     title: "Lagoon Nebula",
+//     url:
+//       "https://cdn.spacetelescope.org/archives/images/wallpaper2/heic1808a.jpg"
+//   },
+//   {
+//     title: "Spirals and Supernovae",
+//     url:
+//       "https://cdn.spacetelescope.org/archives/images/wallpaper2/potw1811a.jpg"
+//   },
+//   {
+//     title: "NGC 6302",
+//     url:
+//       "https://cdn.spacetelescope.org/archives/images/wallpaper2/heic0910h.jpg"
+//   },
+//   {
+//     title: "Orion Nebula",
+//     url:
+//       "https://cdn.spacetelescope.org/archives/images/wallpaper2/heic0601a.jpg"
+//   },
+//   {
+//     title: "A Young Star Sculpts Gas",
+//     url:
+//       "https://cdn.spacetelescope.org/archives/images/wallpaper2/heic0514a.jpg"
+//   },
+//   {
+//     title: "The Whirlpool Galaxy (M51)",
+//     url:
+//       "https://cdn.spacetelescope.org/archives/images/wallpaper2/heic0506a.jpg"
+//   },
+//   {
+//     title: "Stellar Spire in the Eagle Nebula",
+//     url:
+//       "https://cdn.spacetelescope.org/archives/images/wallpaper2/heic0506b.jpg"
+//   },
+//   {
+//     title: "Cosmic Dust Bunnies",
+//     url:
+//       "https://cdn.spacetelescope.org/archives/images/wallpaper2/opo0511a.jpg"
+//   },
+//   {
+//     title: "Stellar Outburst",
+//     url:
+//       "https://cdn.spacetelescope.org/archives/images/wallpaper2/heic0503a.jpg"
+//   },
+//   {
+//     title: "NGC 1300",
+//     url:
+//       "https://cdn.spacetelescope.org/archives/images/wallpaper2/opo0501a.jpg"
+//   },
+//   {
+//     title: "Runaway Galaxy",
+//     url:
+//       "https://cdn.spacetelescope.org/archives/images/wallpaper2/heic0206a.jpg"
+//   },
+//   {
+//     title: "Birth of a Star",
+//     url:
+//       "https://cdn.spacetelescope.org/archives/images/wallpaper2/heic0206c.jpg"
+//   },
+//   {
+//     title: "The Merging of Two Galaxies",
+//     url:
+//       "https://cdn.spacetelescope.org/archives/images/wallpaper2/heic0206b.jpg"
+//   },
+//   {
+//     title: "The Red Spider Nebula",
+//     url:
+//       "https://cdn.spacetelescope.org/archives/images/wallpaper2/heic0109a.jpg"
+//   },
+//   {
+//     title: "Light and Shadow in the Carina Nebula",
+//     url:
+//       "https://cdn.spacetelescope.org/archives/images/wallpaper2/opo0006a.jpg"
+//   },
+//   {
+//     title: "Saturn in Natural Colors",
+//     url:
+//       "https://cdn.spacetelescope.org/archives/images/wallpaper2/opo9828c.jpg"
+//   },
+//   {
+//     title: "Messier 106",
+//     url:
+//       "https://cdn.spacetelescope.org/archives/images/wallpaper2/heic1302a.jpg"
+//   },
+//   {
+//     title: "Close-up of the Tarantula",
+//     url:
+//       "https://cdn.spacetelescope.org/archives/images/wallpaper2/heic1105a.jpg"
+//   },
+//   {
+//     title: "Flocculent Spiral NGC 2841",
+//     url:
+//       "https://cdn.spacetelescope.org/archives/images/wallpaper2/heic1104a.jpg"
+//   }
+// ];

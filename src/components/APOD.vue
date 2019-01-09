570c7c7d00615eebfcebe528a746b5bf62f0620e
chris <christopherchateau@gmail.com>
2019-01-08 17:15:38 -0700

    Add mock data to apodData and import to APOD component

--- src/components/APOD.vue
+++ src/components/APOD.vue
@@ -1,14 +1,30 @@
 <template>
   <div class="apod">
     <h3>apod</h3>
+    
   </div>
 </template>
 
 <script>
+import apiKeys from "../../apiKeys";
+import data from "../helper/apodData";
+
 export default {
   name: "apod",
+
+  async mounted() {
+    // const response = await fetch(
+    //   `https://api.nasa.gov/planetary/apod?api_key=${apiKeys.private}`
+    // );
+    // const data = await response.json();
+
+    this.apodData = data;
+  },
+
   data() {
-    return {};
+    return {
+      apodData: []
+    };
   }
 };
 </script>
---
+++ src/helper/apodData.js
@@ -0,0 +1,35 @@
+export default [
+  {
+    copyright: "Juan Carlos Casado",
+    date: "2019-01-07",
+    explanation:
+      "This was an unusual night to look in the direction of the Bull. The constellation Taurus is always well known for hosting two bright star clusters -- the Pleaides, visible on the right, and the comparatively diffuse Hyades, visible on the left.  This night last month, however, was atypically the peak of the Geminid meteor shower, and so several meteors were caught shooting through the constellation with parallel trails. More unusually still, Comet Wirtanen was drifting through the constellation, here appearing near the image bottom surrounded by a greenish coma. The comet was near its brightest as it sped past the Earth. The orange star on the upper left is Aldebaran, considered to be the eye of the Bull. Aldebaran is the brightest star in Taurus and the 15th brightest star in the sky.  The featured image is a combination of nearly 800 exposures taken from the Spanish village Albanyà.    Follow APOD on: Instagram, Facebook,  Reddit, or Twitter",
+    hdurl: "https://apod.nasa.gov/apod/image/1901/Geminids46P_jcc_2000.jpg",
+    media_type: "image",
+    service_version: "v1",
+    title: "Stars, Meteors, and a Comet in Taurus",
+    url: "https://apod.nasa.gov/apod/image/1901/Geminids46P_jcc_1080.jpg"
+  },
+
+  {
+    copyright: "Vikas ChanderH.E.S.S. Collaboration Music: Immersive Music",
+    date: "2019-01-08",
+    explanation:
+      "They may look like modern mechanical dinosaurs but they are enormous swiveling eyes that watch the sky. The High Energy Stereoscopic System (H.E.S.S.) Observatory is composed of four 12-meter reflecting-mirror telescopes surrounding a larger telescope housing a 28-meter mirror. They are designed to detect strange flickers of blue light -- Cherenkov radiation --emitted when charged particles move slightly faster than the speed of light in air.  This light is emitted when a gamma ray from a distant source strikes a molecule in Earth's atmosphere and starts a charged-particle shower.  H.E.S.S. is sensitive to some of the highest energy photons (TeV) crossing the universe.  Operating since 2003 in Namibia, H.E.S.S. has searched for dark matter and has discovered over 50 sources emitting high energy radiation including supernova remnants and the centers of galaxies that contain supermassive black holes.  Pictured last September, H.E.S.S. telescopes swivel and stare in time-lapse sequences shot in front of our Milky Way Galaxy and the Magellanic Clouds -- as the occasional Earth-orbiting satellite zips by.   Open Science: Browse 1,800+ codes in the Astrophysics Source Code Library",
+    media_type: "video",
+    service_version: "v1",
+    title: "HESS Telescopes Explore the High-Energy Sky",
+    url: "https://apod.nasa.gov/apod/image/1901/Jan3yutu2CNSA1024.jpg"
+  },
+
+  {
+    date: "2019-01-04",
+    explanation:
+      "Ultima Thule is the most distant world explored by a spacecraft from Earth. In the dim light 6.5 billion kilometers from the Sun, the New Horizons spacecraft captured these two frames 38 minutes apart as it sped toward the Kuiper belt world on January 1 at 51,000 kilometers per hour. A contact binary, the two lobes of Ultima Thule rotate together once every 15 hours or so. Shown as a blinking gif, the rotation between the frames produces a tantalizing 3D perspective of the most primitive world ever seen. Dubbed separately by the science team Ultima and Thule, the larger lobe Ultima, is about 19 kilometers in diameter. Smaller Thule is 14 kilometers across.",
+    hdurl: "https://apod.nasa.gov/apod/image/1901/UT-blink_3d_a.gif",
+    media_type: "image",
+    service_version: "v1",
+    title: "Ultima Thule Rotation Gif",
+    url: "https://apod.nasa.gov/apod/image/1901/UT-blink_3d_a.gif"
+  }
+];

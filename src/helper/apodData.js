370b63449b6a3cc932a0f06b9dbcb24c6e8308af
chris <christopherchateau@gmail.com>
2019-01-08 22:44:09 -0700

    Add additional mock data cause NASA API worked for a couple mins

--- src/helper/apodData.js
+++ src/helper/apodData.js
@@ -21,6 +21,26 @@ export default [
     url: "https://apod.nasa.gov/apod/image/1901/Geminids46P_jcc_1080.jpg"
   },
   {
+    date: "2019-01-06",
+    explanation:
+      "Why are these people shooting a powerful laser into the center of our Galaxy?  Fortunately, this is not meant to be the first step in a Galactic war.  Rather, astronomers at the Very Large Telescope (VLT) site in Chile are trying to measure the distortions of Earth's ever changing atmosphere.  Constant imaging of high-altitude atoms excited by the laser -- which appear like an artificial star -- allow astronomers to instantly measure atmospheric blurring.  This information is fed back to a VLT telescope mirror which is then slightly deformed to minimize this blurring.  In this case, a VLT was observing our Galaxy's center, and so Earth's atmospheric blurring in that direction was needed.  As for inter-galaxy warfare, when viewed from our Galaxy's center, no casualties are expected.  In fact, the light from this powerful laser would combine with light from our Sun to together appear only as bright as a faint and distant star.    APOD in other languages: Arabic, Catalan, Chinese (Beijing), Chinese (Taiwan), Croatian, Czech, Dutch, German, French, French, Hebrew, Indonesian, Japanese, Korean, Montenegrin, Polish, Russian, Serbian, Slovenian and Spanish",
+    hdurl: "https://apod.nasa.gov/apod/image/1901/vltlaser_beletsky_1400.jpg",
+    media_type: "image",
+    service_version: "v1",
+    title: "A Laser Strike at the Galactic Center",
+    url: "https://apod.nasa.gov/apod/image/1901/vltlaser_beletsky_960.jpg"
+  },
+  {
+    date: "2019-01-05",
+    explanation:
+      "On January 3, the Chinese Chang'e-4 spacecraft made the first successful landing on the Moon's farside. Taken by a camera on board the lander, this image is from the landing site inside Von Karman crater. It shows the desk-sized, six-wheeled Yutu 2 (Jade Rabbit 2) rover as it rolled down lander ramps and across the surface near local sunrise and the start of the two week long lunar day. Ripe for exploration, Von Karman crater itself is 186 kilometers in diameter. It lies within the Moon's old and deep South Pole-Aitken impact basin with some of the most ancient and least understood lunar terrains. To bridge communications from the normally hidden hemisphere of the Moon, China launched a relay satellite, Queqiao, in May of 2018 in to an orbit beyond the lunar farside.",
+    hdurl: "https://apod.nasa.gov/apod/image/1901/Jan3yutu2CNSA.jpg",
+    media_type: "image",
+    service_version: "v1",
+    title: "Yutu 2 on the Farside",
+    url: "https://apod.nasa.gov/apod/image/1901/Jan3yutu2CNSA1024.jpg"
+  },
+  {
     date: "2019-01-04",
     explanation:
       "Ultima Thule is the most distant world explored by a spacecraft from Earth. In the dim light 6.5 billion kilometers from the Sun, the New Horizons spacecraft captured these two frames 38 minutes apart as it sped toward the Kuiper belt world on January 1 at 51,000 kilometers per hour. A contact binary, the two lobes of Ultima Thule rotate together once every 15 hours or so. Shown as a blinking gif, the rotation between the frames produces a tantalizing 3D perspective of the most primitive world ever seen. Dubbed separately by the science team Ultima and Thule, the larger lobe Ultima, is about 19 kilometers in diameter. Smaller Thule is 14 kilometers across.",
@@ -29,5 +49,37 @@ export default [
     service_version: "v1",
     title: "Ultima Thule Rotation Gif",
     url: "https://apod.nasa.gov/apod/image/1901/UT-blink_3d_a.gif"
+  },
+  {
+    date: "2019-01-03",
+    explanation:
+      "On January 1 New Horizons encountered the Kuiper Belt object nicknamed Ultima Thule. Some 6.5 billion kilometers from the Sun, Ultima Thule is the most distant world ever explored by a spacecraft from Earth. This historic image, the highest resolution image released so far, was made at a range of about 28,000 kilometers only 30 minutes before the New Horizons closest approach. Likely the result of a gentle collision shortly after the birth of the Solar System, Ultima Thule is revealed to be a contact binary, two connected sphere-like shapes held in contact by mutual gravity. Dubbed separately by the science team Ultima and Thule, the larger lobe Ultima is about 19 kilometers in diameter. Smaller Thule is 14 kilometers across.  News: New Horizons science results briefing",
+    hdurl: "https://apod.nasa.gov/apod/image/1901/20190102UltimaThule-pr.png",
+    media_type: "image",
+    service_version: "v1",
+    title: "Ultima and Thule",
+    url: "https://apod.nasa.gov/apod/image/1901/20190102UltimaThule-pr600.jpg"
+  },
+  {
+    copyright: "Francesco Antonucci",
+    date: "2019-01-02",
+    explanation:
+      "The Great Nebula in Orion is an intriguing place.  Visible to the unaided eye, it appears as a small fuzzy patch in the constellation of Orion. But this image, an illusory-color four-panel mosaic taken in different bands of infrared light with the Earth orbiting WISE observatory, shows the Orion Nebula to be a bustling  neighborhood of recently formed stars, hot gas, and dark dust.  The power behind much of the Orion Nebula (M42) is the stars of the Trapezium star cluster, seen near the center of the featured image. The orange glow surrounding the bright stars pictured here is their own starlight reflected by intricate dust filaments that cover much of the region.  The current  Orion Nebula cloud complex, which includes the Horsehead Nebula, will slowly disperse over the next 100,000 years.   News: New Horizons Results from Ultima Thule",
+    hdurl:
+      "https://apod.nasa.gov/apod/image/1901/orionred_WISEantonucci_1824.jpg",
+    media_type: "image",
+    service_version: "v1",
+    title: "The Orion Nebula in Infrared from WISE",
+    url: "https://apod.nasa.gov/apod/image/1901/orionred_WISEantonucci_960.jpg"
+  },
+  {
+    date: "2019-01-01",
+    explanation:
+      "This floating ring is the size of a galaxy.  In fact, it is a galaxy -- or at least part of one: the photogenic Sombrero Galaxy, one of the largest galaxies in the nearby Virgo Cluster of Galaxies.  The dark band of dust that obscures the mid-section of the Sombrero Galaxy in optical light actually glows brightly in infrared light.  The featured image, digitally sharpened, shows the infrared glow, recently recorded by the orbiting Spitzer Space Telescope, superposed in false-color on an existing image taken by NASA's Hubble Space Telescope in optical light. The Sombrero Galaxy, also known as M104, spans about 50,000 light years across and lies 28 million light years away.  M104 can be seen with a small telescope in the direction of the constellation Virgo.   News: New Horizons Spacecraft Passes Ultima Thule",
+    hdurl: "https://apod.nasa.gov/apod/image/1901/sombrero_spitzer_3000.jpg",
+    media_type: "image",
+    service_version: "v1",
+    title: "The Sombrero Galaxy in Infrared",
+    url: "https://apod.nasa.gov/apod/image/1901/sombrero_spitzer_1080.jpg"
   }
 ];
